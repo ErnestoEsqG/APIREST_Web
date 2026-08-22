7f775606d1452d2ad2ae3845df72cc6689ba93c6
ErnestoEsqG <ernestoesq14@gmail.com>
2026-08-22 13:34:47 -0600

    Creacion del CRUD (falta Delete)

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -5,20 +5,70 @@ class StudentsController {
 
     }
     consult(req, res){
-        res.json({msg: 'Consulting students from class'});
+        try{
+            db.query("SELECT * FROM students",
+                (err, rows) => {
+                    if(err) {
+                        res.status(400).send(err);
+                    }
+                    res.status(200).json(rows);
+                });
+        } catch (err) {
+            res.status(500).send(err.message);
+        }
+
     }
 
     consultDetail(req, res){
         const { id } = req.params;
-        res.json({msg: `Consulting one student from class with id ${id}`});
+        try{
+            db.query("SELECT * FROM students WHERE id = ?", [id],
+                (err, rows) => {
+                    if(err) {
+                        res.status(400).send(err);
+                    }
+                    res.status(200).json(rows[0]);
+                });
+        } catch (err) {
+            res.status(500).send(err.message);
+        }
     }
 
     input(req, res){
-        res.json({msg: 'Adding students from class'});
+        try{
+            const { dni, name, lastname, email } = req.body;
+            db.query("INSERT INTO courses.students\n" +
+                "(id, dni, name, lastname, email)\n" +
+                "VALUES(NULL, ?, ?, ?, ?);",
+                [dni, name, lastname, email],(err, rows) => {
+                if(err){
+                    res.status(400).send(err);
+                }
+                res.status(201).json({ id: rows.insertId });
+                });
+        } catch (err) {
+            res.status(500).send(err.message);
+        }
     }
 
     update(req, res){
-        res.json({msg: 'Update student from class'});
+        const { id } = req.params;
+        try{
+            const { dni, name, lastname, email } = req.body;
+            db.query(`UPDATE courses.students 
+             SET dni = ?, name = ?, lastname = ?, email = ? 
+             WHERE id = ?`,
+                [dni, name, lastname, email, id],(err, rows) => {
+                    if(err){
+                        res.status(400).send(err);
+                    }
+                    if(rows.affectedRows == 1)
+                    res.status(200).json({  respuesta: 'Registro actualizado exitosamente'});
+                });
+        } catch (err) {
+            res.status(500).send(err.message);
+        }
+
     }
 
     delete(req, res){
